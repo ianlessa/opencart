74de5b9000f26a1ed706775ed6e04e7448a680f4
felipebool <bolzin@gmail.com>
2017-08-18 18:18:40 -0300

    :art: create a class to manage boleto settings

--- system/library/mundipagg/src/Controller/CreditCardSettings.php
+++ system/library/mundipagg/src/Controller/CreditCardSettings.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: felipe
- * Date: 8/18/17
- * Time: 4:49 PM
- */
 
 namespace Mundipagg\Controller;
-
 
 class CreditCardSettings
 {
@@ -23,7 +16,7 @@ class CreditCardSettings
         return $this->openCart->config->get('payment_mundipagg_credit_card_status');
     }
 
-    public function isCreditCardEnabled() {
+    public function isEnabled() {
         return $this->getStatus() == '1';
     }
 
