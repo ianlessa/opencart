5ce58de9b030f21701cffec3f699939f007e787f
Vitor Mattos <vitor.mattos@phprio.org>
2017-08-25 11:53:44 -0300

    Code improvement

--- RoboFile.php
+++ RoboFile.php
@@ -44,20 +44,22 @@ class RoboFile extends \Robo\Tasks
         }
         $this->taskDeleteDir(getenv('OC_ROOT'))->run();
         $filename = getenv('OPENCART_VERSION') . '-OpenCart.zip';
+        $this->downloadOpenCart();
+        $this->extractOpenCart($filename);
+    }
+
+    private function downloadOpenCart(string $filename)
+    {
         $url = 'https://github.com/opencart/opencart/releases/download/' . getenv('OPENCART_VERSION') . '/' . $filename;
-        if (!file_exists($filename)) {
-            $headers = get_headers($url, 1);
-            $filesize = $headers['Content-Length'];
+        $headers = get_headers($url, 1);
+        $filesize = $headers['Content-Length'];
+        if (!file_exists($filename) || filesize($filename) != $filedize) {
             $remote = fopen($url, 'r');
             $local = fopen($filename, 'x');
-            $this->startProgressIndicator();
-            $read_bytes = 0;
-            $this->steps = $filesize / 1024;
-            $this->showProgressIndicator();
-            $progress = new ProgressBar($this->output(), $this->steps);
+            $progress = new ProgressBar($this->output(), $filesize / 1024);
             $progress->start();
-            $progress->setMessage('Downloading OpenCart '.getenv('OPENCART_VERSION'));
             $progress->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s%');
+            $read_bytes = 0;
             while (!feof($remote)) {
                 $buffer = fread($remote, 1024);
                 fwrite($local, $buffer);
@@ -68,20 +70,22 @@ class RoboFile extends \Robo\Tasks
             fclose($remote);
             fclose($local);
         }
-        if (!is_dir(getenv('OC_ROOT'))) {
-            $zip = new ZipArchive;
-            $res = $zip->open('./'.$filename);
-            if ($res !== true) {
-                throw new Exception('Failed to open ./' . $filename);
-            }
-            $tmpPath = $this->collectionBuilder()->tmpDir();
-            $zip->extractTo($tmpPath);
-            $this->taskFilesystemStack()->mkdir(getenv('OC_ROOT'));
-            $this->taskMirrorDir([
-                $tmpPath.DIRECTORY_SEPARATOR.'upload' => getenv('OC_ROOT')
-            ])->run();
-            $this->taskDeleteDir($tmpPath)->run();
+    }
+
+    private function extractOpenCart(string $filename)
+    {
+        $zip = new ZipArchive;
+        $res = $zip->open('./'.$filename);
+        if ($res !== true) {
+            throw new Exception('Failed to open ./' . $filename);
         }
+        $tmpPath = $this->collectionBuilder()->tmpDir();
+        $zip->extractTo($tmpPath);
+        $this->taskFilesystemStack()->mkdir(getenv('OC_ROOT'));
+        $this->taskMirrorDir([
+            $tmpPath.DIRECTORY_SEPARATOR.'upload' => getenv('OC_ROOT')
+        ])->run();
+        $this->taskDeleteDir($tmpPath)->run();
     }
 
     private function filesToRunTests()
@@ -115,13 +119,14 @@ class RoboFile extends \Robo\Tasks
         }
         $this->taskFileSystemStack()
             ->chmod(getenv('OC_ROOT'), 0777, 0000, true)->run();
-        $script_install = getenv('OC_ROOT').'install/cli_install.php';
-        $define_dir_storage = "define('DIR_STORAGE', '".__DIR__."/system/storage/');";
-        file_put_contents($script_install, str_replace(
-            "error_reporting(E_ALL);\r\n",
-            "error_reporting(E_ALL);\r\n".$define_dir_storage,
-            file_get_contents($script_install)
-        ));
+
+        $this->taskReplaceInFile(getenv('OC_ROOT').'install/cli_install.php')
+            ->from("error_reporting(E_ALL);\r\n")
+            ->to(
+                "error_reporting(E_ALL);\r\n" .
+                "define('DIR_STORAGE', '".__DIR__."/system/storage/');"
+            )
+            ->run();
 
         $this->taskConcat([
             getenv('OC_ROOT') . 'tmp_setup.php',
@@ -129,31 +134,37 @@ class RoboFile extends \Robo\Tasks
         ])
         ->to(getenv('OC_ROOT').'install/cli_install.php')
         ->run();
+
         $install = $this->taskExec('php')->arg(getenv('OC_ROOT').'install/cli_install.php')->arg('install');
         foreach ($this->opencart_config as $option => $value) {
             $install->option($option, $value);
         }
         $install->rawArg('> /dev/null');
         $install->run();
+
         $this->taskMirrorDir([
             getenv('OC_ROOT') . 'system'.DIRECTORY_SEPARATOR.'storage' => __DIR__.DIRECTORY_SEPARATOR.'storage'
         ])->run();
+
         $this->taskDeleteDir(getenv('OC_ROOT') . 'system'.DIRECTORY_SEPARATOR.'storage')->run();
-        file_put_contents(getenv('OC_ROOT').DIRECTORY_SEPARATOR.'config.php', str_replace(
-            "define('DIR_STORAGE', DIR_SYSTEM . 'storage/');",
-            "define('DIR_STORAGE', '".__DIR__.DIRECTORY_SEPARATOR."storage/');",
-            file_get_contents(getenv('OC_ROOT').DIRECTORY_SEPARATOR.'config.php')
-        ));
-        file_put_contents(getenv('OC_ROOT').DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'config.php', str_replace(
-            "define('DIR_STORAGE', DIR_SYSTEM . 'storage/');",
-            "define('DIR_STORAGE', '".__DIR__.DIRECTORY_SEPARATOR."storage/');",
-            file_get_contents(getenv('OC_ROOT').DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'config.php')
-        ));
-        file_put_contents($script_install, str_replace(
-            "error_reporting(E_ALL);\r\n".$define_dir_storage,
-            "error_reporting(E_ALL);\r\n",
-            file_get_contents($script_install)
-        ));
+
+        $this->taskReplaceInFile(getenv('OC_ROOT').DIRECTORY_SEPARATOR.'config.php')
+            ->from("define('DIR_STORAGE', DIR_SYSTEM . 'storage/');")
+            ->to("define('DIR_STORAGE', '".__DIR__.DIRECTORY_SEPARATOR."storage/');")
+            ->run();
+
+        $this->taskReplaceInFile(getenv('OC_ROOT').DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'config.php')
+            ->from("define('DIR_STORAGE', DIR_SYSTEM . 'storage/');")
+            ->to("define('DIR_STORAGE', '".__DIR__.DIRECTORY_SEPARATOR."storage/');")
+            ->run();
+
+        $this->taskReplaceInFile(getenv('OC_ROOT').'install/cli_install.php')
+            ->from(
+                "error_reporting(E_ALL);\r\n" .
+                "define('DIR_STORAGE', '".__DIR__."/system/storage/');"
+            )
+            ->to("error_reporting(E_ALL);\r\n")
+            ->run();
     }
 
     private function installModuleByModman()
