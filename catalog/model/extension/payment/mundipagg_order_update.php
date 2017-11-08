5c18036f685839d0adf9800ecc60b90793958c4a
Michel Lima <michelpl@yahoo.com.br>
2017-11-08 20:12:36 -0200

    :construction: Add orderdata update model.

--- catalog/model/extension/payment/mundipagg_order_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mundipagg;
-
-
-class ModelExtensionPaymentMundipaggOrderUpdate extends Model
-{
-
-}
---
+++ catalog/model/extension/payment/mundipagg_orderdata_update.php
@@ -0,0 +1,9 @@
+<?php
+
+class ModelExtensionPaymentMundipaggOrderDataUpdate
+{
+    public function updateOrderAmount($orderId, $orderAmount)
+    {
+
+    }
+}
