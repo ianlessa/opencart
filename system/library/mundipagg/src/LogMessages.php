4705b1b2f76db058745eab038a1077b16692e2d6
Michel <michelpl@yahoo.com.br>
2017-12-29 13:58:06 -0200

    :bookmark: Bump version.

--- system/library/mundipagg/src/LogMessages.php
+++ system/library/mundipagg/src/LogMessages.php
@@ -4,7 +4,7 @@ namespace Mundipagg;
 
 class LogMessages
 {
-    const LOG_HEADER = 'Mundipagg Opencart V1.2.4 |';
+    const LOG_HEADER = 'Mundipagg Opencart V1.2.5 |';
     
     /** Error messages */
     const UNKNOWN_ORDER_STATUS = 'Unknown order status received';
