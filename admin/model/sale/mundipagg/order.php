47a7606da54734354534c98ca2685b7f10d6098f
Ian Lessa <ian.lessa1@gmail.com>
2018-01-12 10:27:30 -0200

    :bug: Panel Cancel/Capture now add order history

---
+++ admin/model/sale/mundipagg/order.php
@@ -0,0 +1,15 @@
+<?php
+
+class ModelSaleMundipaggOrder extends Model
+{
+    public function addOrderHistory($order_id, $order_status_id, $comment = '', $notify = false, $override = false)
+    {
+        $sql = "INSERT INTO " . DB_PREFIX . "order_history SET " .
+            "order_id = '" . (int)$order_id . "', " .
+            "order_status_id = '" . (int)$order_status_id . "', " .
+            "notify = '" . (int)$notify . "', " .
+            "comment = '" . $this->db->escape($comment) . "', " .
+            "date_added = NOW()";
+        $this->db->query($sql);
+    }
+}
