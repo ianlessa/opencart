420d1a2a1c9da3ad2b526677bcf13d9c3c59df0f
Michel <michelpl@yahoo.com.br>
2018-02-06 17:14:18 -0200

    :globe_with_meridians: Add portuguese translate.

---
+++ admin/language/pt-br/extension/payment/mundipagg.php
@@ -0,0 +1,83 @@
+<?php
+// Heading
+$_['heading_title'] = 'MundiPagg';
+$_['text_mundipagg'] = '<a href="https://github.com/mundipagg/opencart" target="_blank"><img src="/admin/view/image/mundipagg/mundipagg.png" alt="MundiPagg" title="MundiPagg" style="border: 1px solid #EEEEEE;" /></a>';
+// -----------------------------------------------------------------
+
+// General tab settings
+$_['general'] = [
+    'label' => 'Geral',
+    'payment_title' => 'Título do pagamento',
+    'mapping_section' => 'Mapeamento de campos',
+    'mapping_number' => 'Número',
+    'mapping_complement' => 'Complemento',
+    'api_section' => 'Dados da Api',
+    'api_prod_key' => 'Secret key [produção]',
+    'api_test_key' => 'Secret key [teste]',
+    'api_prod_pub_key' => 'Public key [produção]',
+    'api_test_pub_key' => 'Public key [teste]',
+    'api_test_mode' => 'Modo teste',
+    'module_section' => 'Módulo',
+    'log_section' => 'Log',
+    'select' => 'Selecione'
+];
+// -----------------------------------------------------------------
+
+$_['credit_card'] = [
+    'label' => 'Cartão de crédito',
+    'configure_section' => 'Configuração de cartões',
+    'configure_payment_title' => 'Título do pagamento',
+    'configure_invoice_name' => "Nome na fatura",
+    'configure_operation_type' => 'Tipo da operação',
+    'configure_is_saved_enabled' => 'Habilitar cartões salvos',
+    'configure_enable_two_credit_cards' => 'Habilitar 2 cartões',
+    'configure_auth_label' => 'Autorizar',
+    'configure_auth_capture_label' => 'Autorizar e capturar',
+    'manage_section' => 'Gerenciar cartões',
+    'manage_remove_button' => 'Excluir',
+    'manage_add_card' => 'Adicionar outro cartão',
+    'brand_name' => 'Nome da bandeira',
+    'enable' => 'Habilitar',
+    'max_installments' => 'Número máximo de parcelas',
+    'installments_without_interest' => 'Número máximo de parcelas sem juros',
+    'initial_interest' => 'Juros iniciais',
+    'incremental_interest' => 'Juros incrementais'
+];
+// -----------------------------------------------------------------
+
+$_['boleto'] = [
+    'label' => 'Boleto',
+    'configure_section' => 'Configuração do boleto',
+    'configure_payment_title' => 'Título do pagamento',
+    'configure_name' => "Nome",
+    'configure_select_bank' => 'Banco',
+    'configure_due_at' => 'Número de dias do vencimento do boleto após a data da compra',
+    'configure_instructions' => 'Instruções de pagamento'
+];
+// -----------------------------------------------------------------
+
+// -----------------------------------------------------------------
+
+$_['antifraud'] = [
+    'label' => 'Anti fraude',
+    'configure_section' => 'Configuração de anti fraude',
+    'configure_minval' => 'Valor mínimo do pedido'
+];
+// -----------------------------------------------------------------
+
+$_['logs'] = [
+    'label' => 'Logs',
+    'title' => 'Logs'
+];
+// -----------------------------------------------------------------
+
+$_['misc'] = [
+    'yes' => 'Sim',
+    'no' => 'Não',
+    'on' => 'Ligado',
+    'off' => 'Desligado',
+    'enabled' => 'Habilitado',
+    'button_save' => 'Salvar',
+    'button_cancel' => 'Cancelar',
+    'success' => 'Mundipagg options saved!'
+];
--- catalog/language/pt-br/extension/payment/mundipagg.php
+++ catalog/language/pt-br/extension/payment/mundipagg.php
@@ -1,58 +1,64 @@
 <?php
 $_['boleto'] = [
-      'payment_method_name' => 'Boleto',
-      'title' => 'Pagamento via boleto',
-      'click_to_generate' => 'Clique aqui para gerar seu boleto.',
-      'pending_order_status' => 'Pedido pendente - Aguardando pagamento do boleto.',
-      'instructions' => "Após clicar em 'Clique aqui para gerar seu boleto' "
-            . "você será redirecionado para uma nova janela "
-            . "e poderá fazer o donwload ou a impressão de seu boleto."
+    'payment_method_name' => 'Boleto',
+    'title' => 'Pagamento via boleto',
+    'click_to_generate' => 'Clique aqui para gerar seu boleto.',
+    'pending_order_status' => 'Pedido pendente - Aguardando pagamento do boleto.',
+    'instructions' => "Após clicar em 'Clique aqui para gerar seu boleto' "
+        . "você será redirecionado para uma nova janela "
+        . "e poderá fazer o donwload ou a impressão de seu boleto."
 ];
 
 $_['credit_card'] = [
     'payment_method_name' => 'Cartão de crédito',
     'credit_card_number' => 'Número do cartão',
-    'valid_thru' => 'Válido até',
+    'valid_thru' => 'Validade',
     'brand' => 'Bandeira',
     'holder_name' => 'Nome impresso no cartão',
     'cvv' => 'CVV',
     'no_brands_enabled' => 'Nenhuma bandeira habilitada',
     'installments' => 'Parcelas',
     'without_interest' => 'sem juros',
-    'interest' => 'juros: ',
+    'interest' => 'huros: ',
     'new_credit_card' => 'Novo cartão',
-    'save_creditcard_message' => 'Salvar este cartão para compras futuras',
+    'save_creditcard_message' => 'Salvar este cartão para compras futuras'
 ];
 
 $_['order'] = [
-      'pending' => 'Order pending',
-      'paid' => 'Order paid',
-      'cancel' => 'Order canceled',
-      'void' => 'Order voided',
+    'pending' => 'Pedido pendente',
+    'paid' => 'Pedido pago',
+    'cancel' => 'Pedido cancelado',
+    'void' => 'Pedido cancelado',
 ];
 
 $_['order_history_update'] = [
-    'chargePaid' => 'Charge paid: ',
-    'chargeRefunded' => 'Charge refunded',
-    'chargeOverPaid' => 'Charge overpaid: ',
-    'chargeUnderPaid' => 'Charge underpaid: ',
-    'orderPaid' => 'Order paid ',
-    'orderCanceled' => 'Order canceled ',
-    'of' => ' of '
+    'chargePaid' => 'Cobrança paga: ',
+    'chargeRefunded' => 'Cobrança estornada',
+    'chargeOverPaid' => 'Cobrança paga à maior: ',
+    'chargeUnderPaid' => 'Cobrança paga à menor: ',
+    'chargePaymentFailed' => 'Cobrança com falha no pagamento',
+    'orderPaid' => 'Pedido pago ',
+    'orderCanceled' => 'Pedido cancelado ',
+    'of' => ' de '
 ];
 
 $_['misc'] = [
-    'no_payment_methods_enabled' => 'No payment methods enabled',
-    'continue' => 'Continue'
+    'no_payment_methods_enabled' => 'Nenhum método de pagamento habilitado',
+    'continue' => 'Continuar'
+];
+
+$_['error_message'] = [
+    'unknown_order_status' => 'Ops! Something went wrong. Try again in a few minutes or contact the store administration'
 ];
 
 $_['saved_creditcard'] = [
-    'title' => 'Cartões salvos',
+    'title' => 'Meus cartões',
     'my_creditcards' => 'Meus cartões',
-    'your_saved_creditcards' => 'Meus cartões salvos',
-    'saved_card_confirm_delete' => 'Este cartão será removido. Deseja remover este cartão dos seus cartões salvos?',
+    'account' => 'Conta',
+    'your_saved_creditcards' => 'Meus cartões salvos.',
+    'saved_card_confirm_delete' => 'Esse cartão será removido. Você deseja continuar?',
     'delete_card_wait_message' => 'Aguarde...',
-    'delete_card_button' => 'Remover',
+    'delete_card_button' => 'Excluir',
     'brand' => 'Bandeira',
     'last_four_digits' => 'Últimos quatro dígitos',
     'delete' => 'Excluir'
