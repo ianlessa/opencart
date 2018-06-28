9c1719af509a3b6801e9de6d52934fda256b76a9
Ian Lessa <ian.lessa1@gmail.com>
2018-06-28 15:15:01 -0300

    :construction: added Template Repository

---
+++ system/library/mundipagg/src/Repositories/Template.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mundipagg\Repositories;
+
+use Mundipagg\Aggregates\Template\TemplateRoot;
+
+class TemplateRepository
+{
+    public function save(TemplateRoot $template)
+    {
+
+    }
+
+    public function delete(TemplateRoot $template)
+    {
+
+    }
+
+    public function find($templateId)
+    {
+
+    }
+
+    public function listEntities($limit = 0)
+    {
+
+    }
+}
